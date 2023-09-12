4982801349f58dde86d337988fe235708cbf3b92
manish17salian <manishsalian16@gmail.com>
2023-09-12 14:06:09 +0530

    updated kadanes algorithm

--- Algorithms/Kadane’s_Algorithm.py
+++ Algorithms/Kadane’s_Algorithm.py
@@ -1,7 +1,15 @@
-def maxSubArraySum(self,arr,N):
+def maxSubArraySum(arr):
     ##Your code here
     max_sum = current_sum = arr[0]
+
     for i in range(1, len(arr)):
-        current_sum = max(arr[i], current_sum + arr[i])
-        max_sum = max(max_sum, current_sum)
+        current_sum += arr[i]
+        if current_sum > max_sum:
+            max_sum = current_sum
+        if current_sum < 0:
+            current_sum = 0
+
+    print(max_sum)
     return max_sum
+
+maxSubArraySum([5,-4,-2, 6, -1])
